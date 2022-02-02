5700eb1982e7b9c7f86af53f4d5e67b15c00ff50
Sandra Opone <sopone@twitter.com>
2022-02-01 21:07:38 -0600

    Added code to get and display user input

---
+++ src/main/kotlin/Day1/Main.kt
@@ -0,0 +1,20 @@
+
+
+fun main(args: Array<String>) {
+
+    print("Enter text: ")
+    val inputString = readLine()!! // Enter Welcome to 30 Days of Code!
+    println("Hello, World.")
+    println(inputString)
+
+    /** Java
+    Scanner scan = new Scanner(System.in);
+
+    String inputString = scan.nextLine();
+    scan.close();
+
+    System.out.println("Hello, World.");
+
+    System.out.println(inputString);
+     */
+}
